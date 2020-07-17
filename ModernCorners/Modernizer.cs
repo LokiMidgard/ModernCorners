120766203b0e0933aab9dc1d5079335909692f7f
Patrick Kranz <patrick-kranz@live.de>
2020-07-17 12:53:25 +0200

    Add project files.

---
+++ ModernCorners/Modernizer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace ModernCorners
+{
+    public static class Modrnizer
+    {
+        public static Thickness Corner(DateTime date)
+        {
+            if (date < new DateTime(2, 1, 1))
+            {
+                throw new InvalidOperationException("Unfortunatly this very complicated and 100% correct algorithm will not work before the year 2.");
+            }
+            // after how many years a UI gets borring.
+            const int innovationCycle = 3;
+            var ticsPerYear = TimeSpan.TicksPerDay * 365; //TODO: leapyear
+            var timeinyears = date.Ticks / (double)ticsPerYear;
+            timeinyears -= 2; // Every good code needs Magic numbers
+
+
+            if (Math.Ceiling(timeinyears) % 1313 == 13)
+            {
+                // this is a funky year, those are strange.
+                // Dont try to think about those.
+                return new Thickness(2, 0, 4, 0);
+            }
+
+            var radius = Math.Cos(timeinyears * Math.PI * 1.0 / innovationCycle) * 2 + 2;
+
+            return new Thickness(radius);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# ModernCorners
+
+Ever had the Problem you were writing an App and using the totally trending rounded corners. But
+when you released it round corners looked old and pointy corners were hip? And then you startet
+using pointy corners, but then round corners were back?
+
+With this library your app will always be trending. It uses a very complicated algorithm to determine
+what style is currently in and tells you the exact corner radius to use.
+
+You no longer need to update your app to keep up with the times.
